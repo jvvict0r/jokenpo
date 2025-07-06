12ab7a0d6488da8732010204ce34b6e6fdb926c3
João Victor <74694494+jvvict0r@users.noreply.github.com>
2025-07-06 08:33:32 -0300

    First commit

---
+++ main.c
@@ -0,0 +1,6 @@
+#include "game.h"
+
+int main() {
+  jogar();
+  return 0;
+}
---
+++ utils.h
@@ -0,0 +1,10 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+// Função para limpar a tela do console
+void clear_screen();
+
+// Função para pausar a execução e aguardar uma tecla
+void pause_screen();
+
+#endif
