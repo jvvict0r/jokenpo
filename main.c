c1248a74b21a4c295f2539b150a15c1718cdad9a
DavidSantos-44 <jesus.devid@gmail.com>
2025-07-19 18:42:38 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,33 @@
 #include "game.h"
+#include "gconio.h"
+#include <windows.h>
+
+
+void inicializar_interface();
 
 int main() {
+  inicializar_interface();
   jogar();
+  liberar_memoria();
+
+  clrscr();
+  textbackground(BLACK);
+  textcolor(WHITE);
+  printf("Obrigado por jogar!\n");
   return 0;
+}
+
+
+void inicializar_interface() {
+
+    system("chcp 65001");
+
+    SetConsoleTitleA("Jokenpo - O Jogo");
+
+    system("mode con: cols=90 lines=25");
+
+
+    textbackground(LIGHTCYAN);
+    textcolor(BLACK);
+    clrscr();
 }
